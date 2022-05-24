d5911b831caf6469571532309f5641a9b85fb1ee
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-24 11:54:34 -0300

    Integração com css utilizando module

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,12 @@
+//CSS específico para um componente
+import styles from "./integracao2.module.css"
+
+export default function integracao2(){
+    return(
+        <div id={styles.integracao2}>
+            <div className={styles.vermelha}>Texto #01</div>
+            <div className={styles.azul}>Texto #02</div>
+            <div className={styles.branca}>Texto #03</div>
+        </div>
+    )
+}
